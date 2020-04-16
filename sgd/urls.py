d7d9505f87e4a8ca208b334e21447ee104d47397
Mfoster <matt_foster@gap.com>
2020-04-15 23:47:11 -0700

    renamed stuff

---
+++ sgd/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from sgd import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("sgd/<name>", views.hello_there, name="hello_there"),
+    path("sgd/ml/upload", views.simple_upload, name="simple_upload")
+]
