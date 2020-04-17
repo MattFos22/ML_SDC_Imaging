4d9d915d1bd063764a4032e72e004e7c711d0685
Mfoster <matt_foster@gap.com>
2020-04-16 22:13:41 -0700

    Added really bad car classification model - not uploaded as 84 meg

--- sgd/mlmodels/formula1classifier.py
+++ sgd/mlmodels/formula1classifier.py
@@ -1,33 +1,14 @@
 from fastai.vision import (
     ImageDataBunch,
-    ConvLearner,
+    learner,
     open_image,
     get_transforms,
     models,
 )
-import torch
 
-# def handle_uploaded_file(f):
-#     return 'Its a car man!'
-f1_data = ImageDataBunch.from_name_re(
-    cat_images_path,
-    cat_fnames,
-    r"/([^/]+)_\d+.jpg$",
-    ds_tfms=get_transforms(),
-    size=224,
-)
-
-f1_learner = ConvLearner(f1_data, models.resnet34)
-f1_learner.model.load_state_dict(
-    torch.load("f1_teams_model.pth", map_location="cpu")
-)
-
-def handle_uploaded_file(image):
-    _,_,losses = learner.predict(image)
-    return {
-        "predictions": sorted(
-            zip(cat_learner.data.classes, map(float, losses)),
-            key=lambda p: p[1],
-            reverse=True
-        )
-    }
+def handle_uploaded_file(savedImageLocation):
+    image = open_image(savedImageLocation)
+    path = 'sgd/static'
+    learn = learner.load_learner(path)
+    pred_class = learn.predict(image)
+    return pred_class
